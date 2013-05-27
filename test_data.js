efb7e41c6854e053ac039c0d5467590b29958246
Christian Ramon <rchristian.ramon@gmail.com>
2013-05-26 21:52:21 -0500

    Upd: test data.

--- test_data.js
+++ test_data.js
@@ -4,16 +4,13 @@ obj3={_id:3,name:"Gantz vol 28",description:"Descubre cientos de esferas de Gant
 obj4={_id:4,name:"Rurouni Kenshin vol 28",description:"Desenlace saga Enishi",price:49.00, img:"4.PNG"};
 obj5={_id:5,name:"Dragon Ball Despedida",description:"Final de la hisoria de Toriyama",price:35.00, img:"5.PNG"};
 obj6={name:"Death Note vol 1",description:"Conoce a L y Yagami Light",price:24.00, img:"6.PNG"};
+
 usr={name:"system",password:"personal",type:"admin"};
-usr={name:"carlitos",password:"none",type:"customer"};
-
 db.users.save(usr);
-
 
 db.products.save(obj1);
 db.products.save(obj2);
 db.products.save(obj3);
-
 db.products.save(obj4);
 db.products.save(obj5);
 db.products.save(obj6);
