d3065e7a2d44307e74845d5b9564b71644872b44
Christian Ramon <rchristian.ramon@gmail.com>
2013-05-26 21:39:41 -0500

    Upd: adding images property to test data.

--- test_data.js
+++ test_data.js
@@ -1,10 +1,9 @@
-use shoppingCart
-obj1={_id:1,name:"Saint Seiya Next Dimension vol 1",description:"Continuacion del famoso manga de los 80",price:45.00};
-obj2={_id:2,name:"Saint Seiya Lost Canvas vol 1",description:"Conoce la guerra santa hace 248 anhos con Tenma",price:55.00};
-obj3={_id:3,name:"Gantz vol 28",description:"Descubre cientos de esferas de Gantz en Alemania",price:40.00};
-obj4={_id:4,name:"Rurouni Kenshin vol 28",description:"Desenlace saga Enishi",price:49.00};
-obj5={_id:5,name:"Dragon Ball Despedida",description:"Final de la hisoria de Toriyama",price:35.00};
-obj6={name:"Death Note vol 1",description:"Conoce a L y Yagami Light",price:24.00};
+obj1={_id:1,name:"Saint Seiya Next Dimension vol 1",description:"Continuacion del famoso manga de los 80",price:45.00, img:"1.PNG"};
+obj2={_id:2,name:"Saint Seiya Lost Canvas vol 1",description:"Conoce la guerra santa hace 248 anhos con Tenma",price:55.00, img:"2.PNG"};
+obj3={_id:3,name:"Gantz vol 28",description:"Descubre cientos de esferas de Gantz en Alemania",price:40.00, img:"3.PNG"};
+obj4={_id:4,name:"Rurouni Kenshin vol 28",description:"Desenlace saga Enishi",price:49.00, img:"4.PNG"};
+obj5={_id:5,name:"Dragon Ball Despedida",description:"Final de la hisoria de Toriyama",price:35.00, img:"5.PNG"};
+obj6={name:"Death Note vol 1",description:"Conoce a L y Yagami Light",price:24.00, img:"6.PNG"};
 usr={name:"system",password:"personal",type:"admin"};
 usr={name:"carlitos",password:"none",type:"customer"};
 
